9ccd48d440ea396a99ff5850a2fa6a408d450569
nickisucks <nickihugsvampires@gmail.com>
2025-01-26 15:32:51 -0500

    idk what this is

---
+++ bedroomeyes/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import './assets/app.css'
+
+const renderSection = () => {
+  switch (currentSection) {
+    case 'perception':
+      return <perception />
+  }
+}
+import './app.css'; // Assuming your CSS file is named 'styles.css'
+
+function MyComponent() {
+  return (
+    <div className="my-class"> 
+      {/* Your JSX content here */}
+    </div>
+  );
+}
+import React from 'react';
+import './MyComponent.css'; // Import the CSS file
+
+function MyComponent() {
+  return (
+    <div className="background-image">
+      {/* Your content here */}
+    </div>
+  );
+}
+
+export default MyComponent;
+import styles from './app.module.css';
+
+function App() {
+  return (
+    <div className={styles.container}> 
+      { }
+    </div>
+  );
+}
+
+export default App;
